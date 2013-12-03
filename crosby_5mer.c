3bbe7782e4a38f1c45372e28ae3570488bf1930e
rpurbo <rikky.wenang@gmail.com>
2013-12-03 18:25:34 +0800

    Add arguments and switches parsing.

--- crosby_5mer.c
+++ crosby_5mer.c
@@ -1,3 +1,25 @@
+/*
+ * CROSBY - efficient overlapping paired-end reads merging tool
+ *
+ * CROSBY is a tool to merge paired-end reads which was designed to originate from a fragment that is smaller than 2xread length. 
+ * The design produces two single-end reads that shares several basepairs in the 3'-ends. This enables experimentalist to utilize 
+ * long contiguous reads instead of two separate short reads.
+ *
+ * CROSBY is designed to provide the following advanced features:
+ * 	-  K-mer based extension identification. This feature is faster than brute-force algorithm in that it will only test smaller number
+ * 	   of extension point indicated by k-mer location.
+ * 	-  Automatic quality trimming. CROSBY provides option to automatically trim bases in 3'-end that have quality score lower than 2.
+ * 	-  Statistics test for base conflict resolution. CROSBY employs statistics test to decide which bases to retain in the case of
+ * 	   mismatch between pair.
+ * 	-  Repeat detection and resolution. CROSBY provides a method to detect short repeats in the fragment.
+ * 	-  Rescue mode. CROSBY provides a method to link two reads which shared less than 10bps in their extension point. The method
+ * 	   examines all possible extension within the 10bps and compares it against a dynamic k-mer database.
+ *
+ * CROSBY source code can be downloaded from https://github.com/rpurbo/crosby
+ *
+ * Rikky W. Purbojati
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -60,32 +82,98 @@ void reverse_complement(char*, size_t);
 char complement(char);
 void merge_kmer(struct read_pairs, struct PARAMS);
 	
-int main(){
-	struct PARAMS param;
-	param.KMER = 5;
-	param.ERR_RATIO = 0.25;
-	param.MIN_OVERLAP = 10;
-	
+int main(int argc, char **argv){
+        struct PARAMS param;
+        param.KMER = 5;
+        param.ERR_RATIO = 0.25;
+        param.MIN_OVERLAP = 10;
+
+	// GETTING THE ARGUMENTS
+
+	extern char *optarg; 
+	extern int optind; 
+	int c, err = 0; 
+	int mflag=0, pflag=0, fflag=0; 
+	char *sname = "default_sname", *fname; 
+	static char usage[] = "usage: %s [-x max error ratio] [-L minimum overlap] read1 read2 output_prefix\n"; 
+	while ((c = getopt(argc, argv, "k:x:L:")) != -1) 
+		switch (c) { 
+			case 'k':
+				fflag = 1; 
+				//param.KMER = atoi(optarg);
+				break; 
+			case 'x': 
+				mflag = 1; 
+				param.ERR_RATIO = atof(optarg);
+				break; 
+			case 'L': 
+				pflag = 1; 
+				param.MIN_OVERLAP = atoi(optarg);
+				break; 
+			case '?': 
+				err = 1; 
+				break; 
+		}
+
+	if (err) { 
+		fprintf(stderr, usage, argv[0]); 
+		exit(1); 
+	}
+
+	if((argc-optind) < 3){
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	}
+
+	if(param.ERR_RATIO < 0 || param.ERR_RATIO > 1){
+		fprintf(stderr, "\nError ratio must be between 0 and 1\n\n");
+		fprintf(stderr, usage, argv[0]);
+                exit(1);
+	}
+
+	size_t len = strlen(argv[optind]);
+	char input1[len+1];
+	strncpy(input1,argv[optind],len);
+	input1[len] = '\0';
+	len = strlen(argv[optind+1]);
+        char input2[len+1];
+        strncpy(input2,argv[optind+1],len);	
+	input2[len] = '\0';
+	len = strlen(argv[optind+2]);
+        char output[len+1];
+        strncpy(output,argv[optind+2],len);
+	output[len] = '\0';
+
+	/*
+	printf("%s\n",input1);
+	printf("%s\n",input2);
+	printf("%s\n",output);
+	printf("%f\n",param.ERR_RATIO);	
+
+	exit(0);
+	*/	
+
+	// OPENING THE FILES
+
         FILE * oFile;
-        oFile = fopen ("result.txt","w");
+        oFile = fopen (output,"w");
 	param.outFile= oFile;
 	
 	FILE* fq1;
 	FILE* fq2;
-        //fq1 = fopen("test/test.1.fq","r");
-	//fq2 = fopen("test/test.2.fq","r");
-	//fq1 = fopen("sample_R1.fq","r");
-        //fq2 = fopen("sample_R2.fq","r");
 	
-	fq1 = fopen("/scratch/experiment/reads_sim_acc_len100bp_frag180bp_err2perc.R1.fastq","r");
-        fq2 = fopen("/scratch/experiment/reads_sim_acc_len100bp_frag180bp_err2perc.R2.fastq","r");
-	
+	//fq1 = fopen("/scratch/experiment/reads_sim_acc_len100bp_frag180bp_err2perc.R1.fastq","r");
+        //fq2 = fopen("/scratch/experiment/reads_sim_acc_len100bp_frag180bp_err2perc.R2.fastq","r");
+        fq1 = fopen(input1,"r");
+        fq2 = fopen(input2,"r");	
 	
 	char * line1 = NULL;
 	char * line2 = NULL;
 	size_t len1,len2 = 0;
 	ssize_t num1,num2;
 	
+	// ITERATE THE READS	
+
 	while (((num1 = getline(&line1, &len1, fq1)) != -1) && ((num2 = getline(&line2, &len2, fq2)) != -1)) {
 		
 		char header1[num1];
